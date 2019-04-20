e1a5964ca920251e4a8a090d1816d64f4b86e63d
ukhanko <bobkov.artem.a@yandex.ru>
2019-04-20 12:37:55 +0300

    add BarScaner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Container, Button, Text } from 'native-base';
+import BarcodeScanner from './components/BarScaner';
 
 export default class App extends Component {
   render() {
     return (
       <Container>
-        <Button>
+        {/* <Button>
           <Text>
-            Button
+            Кнопка1
           </Text>
-        </Button>
+        </Button> */}
+        <BarcodeScanner />
       </Container>
     );
   }
